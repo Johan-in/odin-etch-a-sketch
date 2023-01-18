45ee38a65a889b153051f6cc1901672edcb5ae95
Johan-in <senatovi21@gmail.com>
2023-01-19 01:19:30 +0400

    finish the click to draw

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 let color = "black";
+let click = false;
 
 document.addEventListener("DOMContentLoaded", function () {
     createBoard(16);
+
+    document.querySelector("body").addEventListener("click", function(e) {
+          if(e.target.tagName != "BUTTON") {
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if (click) {
+                draw.innerHTML = "Now You Can Draw"
+            }
+            else {
+                draw.innerHTML = "You're Not Allowed To Draw"
+            }
+          }
+    })
 
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener("click", function(){
@@ -43,12 +57,16 @@ function getSize(){
 }
 
 function colorDiv () {
-   if (color == "random") {
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+
+    if (click) {
+
+        if (color == "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }
+        else{
+        this.style.backgroundColor = 'black'
    }
-   else{
-    this.style.backgroundColor = 'black'
-   }
+}
 }
 
 function setColor (colorChoice) {
